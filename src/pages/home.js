2d87843be6991a23aac0900813df93d1c560e9d0
Amit Friedberg <amit.friedberg@fxempire.com>
2020-04-19 22:56:26 +0300

    Initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,105 @@
+// @flow
+import * as React from "react";
+import { format } from "date-fns";
+
+// Base UI components
+import { Card } from "baseui/card";
+import { Block } from "baseui/block";
+import { HeaderNavigation } from "baseui/header-navigation";
+import { StatefulInput } from "baseui/input";
+import { Notification, KIND } from "baseui/notification";
+import Search from "./search";
+
+// redux and fusion helpers
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { prepared } from "fusion-react";
+import { withRPCRedux } from "fusion-plugin-rpc-redux-react";
+
+// types
+import type { ConcertT } from "../redux/concerts";
+
+class Home extends React.Component<
+  {
+    getConcerts: () => void,
+    concerts: { data: ConcertT[], error: ?string }
+  },
+  { search: string }
+> {
+  state = {
+    search: ""
+  };
+  componentDidMount() {
+    // optional re-fetch on the client
+    this.props.getConcerts();
+  }
+  render() {
+    const { concerts } = this.props;
+    if (concerts.error) {
+      return (
+        <Block display="flex" justifyContent="center">
+          <Notification kind={KIND.negative}>{concerts.error}</Notification>
+        </Block>
+      );
+    }
+    return (
+      <React.Fragment>
+        <HeaderNavigation>
+          <StatefulInput
+            overrides={{ After: Search }}
+            placeholder="Concerts in Iceland"
+            onChange={e => this.setState({ search: e.target.value })}
+          />
+        </HeaderNavigation>
+        <Block
+          display="grid"
+          gridTemplateColumns="repeat(auto-fill, minmax(280px, 1fr))"
+          justifyItems="center"
+          gridGap="scale1000"
+          margin="scale1000"
+        >
+          {concerts.data &&
+            concerts.data
+              .filter(concert =>
+                concert.name
+                  .toLowerCase()
+                  .includes(this.state.search.toLowerCase())
+              )
+              .map(concert => (
+                <Card
+                  headerImage={concert.imageSource}
+                  title={concert.name}
+                  key={concert.eventDateName + concert.dateOfShow}
+                  overrides={{
+                    Root: {
+                      style: { maxWidth: "280px", justifySelf: "center" }
+                    }
+                  }}
+                >
+                  📅 {format(concert.dateOfShow, "MM/DD/YYYY hh:mm A")}
+                  <br />
+                  📍 {concert.eventHallName}
+                </Card>
+              ))}
+        </Block>
+      </React.Fragment>
+    );
+  }
+}
+
+const hoc = compose(
+  // generates Redux actions and
+  // a React prop for the `getConcerts` RPC call
+  withRPCRedux("getConcerts"),
+  // expose the Redux state to React props
+  connect(({ concerts }) => ({ concerts })),
+  // invokes the passed in method on component hydration
+  prepared(props => {
+    if (props.concerts.loading || props.concerts.data.length) {
+      return Promise.resolve();
+    }
+    return props.getConcerts();
+  })
+);
+
+export default hoc(Home);
---
+++ src/rpc/index.js
@@ -0,0 +1,24 @@
+// @flow
+import { ResponseError } from "fusion-plugin-rpc-redux-react";
+
+export default {
+  getConcerts: async () => {
+    try {
+      const response = await fetch("https://apis.is/concerts");
+
+      // TEST Error: Network
+      // const response = await fetch("http://exampleofnetworkerror.com");
+
+      // TEST Error: Status code is not 200
+      //const response = await fetch("http://httpstat.us/500");
+
+      if (response.status == 200) {
+        const json = await response.json();
+        return json.results;
+      }
+      throw response.statusText;
+    } catch (e) {
+      throw new ResponseError(e);
+    }
+  }
+};
